7b71cc9ecce5dda616bfe5577b539d0fc0e054d6
RayOsLei <lei.lei@ostorage.com.cn>
2017-07-04 15:19:57 +0800

    root commit

---
+++ tester_env.sh
@@ -0,0 +1,8 @@
+export ST_USER=test:tester
+export ST_KEY=testing
+# 不使用负载均衡, proxy server为http模式
+export ST_AUTH=http://192.168.21.220:5000/auth/v1.0
+
+
+
+echo -e  "\e[5;31;43mUser:test v1.0 export complete!\e[0m"
